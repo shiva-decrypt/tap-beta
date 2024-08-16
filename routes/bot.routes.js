942bf192cab9e874da47968b7e8ae0fd6d763ac7
shiva kumar <shiva.kumar@decryptblock.com>
2024-08-16 19:08:56 +0530

    init

--- routes/bot.routes.js
+++ routes/bot.routes.js
@@ -8,6 +8,8 @@ router.post("/claim-task-reward", verifyTgUser, mainController.completTask)
 router.post("/claim-refral-reward", verifyTgUser, mainController.completeRefferalTask)
 router.post("/add-clicks", verifyTgUser,mainController.clickUpdate)
 router.post("/leaderboard",verifyTgUser,mainController.getLeaderBoard)
+router.post("/upgrade-energylimit",verifyTgUser,mainController.UpgradeUserEnergy)
+router.post("/upgrade-clicker", verifyTgUser, mainController.UpgradeUserMultiply)
 router.put("/update-user", verifyTgUser)
 
 export default router;
